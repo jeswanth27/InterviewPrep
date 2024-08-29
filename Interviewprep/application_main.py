fb67e7269110bcc66cbf869828780870e933ee6a
jeswanth27 <jeswanth27@gmail.com>
2024-08-29 22:27:39 +0530

    commintinng

--- Interviewprep/application_main.py
+++ Interviewprep/application_main.py
@@ -11,7 +11,7 @@ if __name__=="__main__":
     print("create spark session")
     spark=get_spark_session(job_run_env)
     print(f"Application Id: {spark.sparkContext.applicationId}")
-    print("create customer dataframe")
+    print("create customer dataframe main")
     df=DataReader.read_customers(spark,job_run_env)
     df.show()
     exit(0)
